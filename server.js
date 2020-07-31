cdecea4409c2ac2fd7bd3dda74fa2da4fc110fcc
Michael Huang MacBookAir <yenming0212@gmail.com>
2020-07-31 18:32:22 +0800

    modify 0731

--- server.js
+++ server.js
@@ -4,8 +4,16 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+// Serve static assets
+// app.use(express.static(path.resolve(__dirname, 'demos')));
+app.use(express.static(path.resolve(__dirname, '')));
+
+
+
 
 // Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
