dcfa9506c196976f315c4bd857278927a0032d12
Michael Huang MacBookAir <yenming0212@gmail.com>
2020-08-09 16:34:28 +0800

    modify port to 400

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser	= require('body-parser');
 const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 4000;
 
 // app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: true}));
